4917317d8c31889bd87a667d34383bfe79cc6c1a
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-11-29 14:37:04 +0300

    recreated the class components with using hooks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
 import MovieCard from "./components/MovieCard";
 import MovieForm from "./components/MovieForm";
+import { useState } from "react";
 
 //we use state because we'll have input field. So we change the function component to class component
 
-class App extends Component {
-  state = {
-    selectedMovieTitle: "Doctor Strange",
+const App = () => {
+  // state = {
+  //   selectedMovieTitle: "Doctor Strange",
+  // };
+
+  const [selectedMovieTitle, setSelectedMovieTitle] =
+    useState("Doctor Strange");
+
+  const changeSelectedMovieTitle = (newMovieTitle) => {
+    // this.setState({
+    //   selectedMovieTitle: newMovieTitle,
+    // });
+    setSelectedMovieTitle(newMovieTitle);
   };
 
-  changeSelectedMovieTitle = (newMovieTitle) => {
-    this.setState({
-      selectedMovieTitle: newMovieTitle,
-    });
-  };
-  render() {
-    return (
-      <div className="App">
-        <Container>
-          <Row className="justify-content-center mt-4">
-            <Col xs={12} md={6}>
-              <h1>Movie Info App!</h1>
-              <MovieForm
-                selectedMovieTitle={this.state.selectedMovieTitle}
-                changeSelectedMovieTitle={this.changeSelectedMovieTitle}
-              />
-            </Col>
-          </Row>
-          <Row className="justify-content-center mt-4">
-            <Col xs={12} md={6}>
-              <MovieCard selectedMovieTitle={this.state.selectedMovieTitle} />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <Container>
+        <Row className="justify-content-center mt-4">
+          <Col xs={12} md={6}>
+            <h1>Movie Info App!</h1>
+            <MovieForm
+              selectedMovieTitle={selectedMovieTitle}
+              changeSelectedMovieTitle={changeSelectedMovieTitle}
+            />
+          </Col>
+        </Row>
+        <Row className="justify-content-center mt-4">
+          <Col xs={12} md={6}>
+            <MovieCard selectedMovieTitle={selectedMovieTitle} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,25 +1,33 @@
 //because movieCard is a component that will be in charge of fetching
-import { Component } from "react";
+// import { Component } from "react";
 import { Card, Spinner } from "react-bootstrap";
+import { useEffect, useState } from "react";
 //data from the omdbAPI, this already tells me that MovieCard needs to be
 //created as a Class Component
 
-class MovieCard extends Component {
+const MovieCard = (props) => {
   //this.props.selectedMovieTitle is the movie chosen in dropdown menu
 
   //every time you fetch something from the internet and  you need your interface
   //to use it, put it into the statE: after setState your interface will render
   //again to show the dew data
-  state = {
-    selectedMovieObject: null,
-    isLoading: true,
-    //null is the perfect initial value for an OBJECT STATE PROPERT!!!!
-  };
+  // state = {
+  //   selectedMovieObject: null,
+  //   isLoading: true,
+  //   //null is the perfect initial value for an OBJECT STATE PROPERTY!!!!
+  // };
 
-  componentDidMount() {
-    //stuff lauched inside componentDidMont will execute one time upon launch!
-    this.fetchMovieData();
-  }
+  const [selectedMovieObject, setSelectedMovieObject] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  // componentDidMount() {
+  //   //stuff lauched inside componentDidMont will execute one time upon launch!
+  //   this.fetchMovieData();
+  // }
+
+  useEffect(() => {
+    fetchMovieData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   //componentDidMount is perfect for fetching data initially, to fill up our
   // component with dynamic content for its initial load
@@ -32,74 +40,77 @@ class MovieCard extends Component {
   //you need to work with componentDidUpdate, whic is a lifecylce method
   //that is being automatically executed just AFTER the detection of an update
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("MOVIECARD HAS BEEN UPDATED");
-    console.log("prevProps:", prevProps);
-    console.log("currentProps:", this.props);
-    // this.fetchMovieData(); //You can't do that, it gives infinite loop,
-    //because our function sets state, and thats fired componentDidUpdated again
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log("MOVIECARD HAS BEEN UPDATED");
+  //   console.log("prevProps:", prevProps);
+  //   console.log("currentProps:", this.props);
+  //   // this.fetchMovieData(); //You can't do that, it gives infinite loop,
+  //   //because our function sets state, and thats fired componentDidUpdated again
 
-    //what can we do to solve the problem, we can use this
-    // we^d like this this componentDidUpdate method to be launched in this situation
-    //whenever the title changes from the props, but not when the state is set!
+  //   //what can we do to solve the problem, we can use this
+  //   // we^d like this this componentDidUpdate method to be launched in this situation
+  //   //whenever the title changes from the props, but not when the state is set!
 
-    //every time you use componentDidUpdate, you have to put a constraint!
-    //you want to pull handbrake
-    if (prevProps.selectedMovieTitle !== this.props.selectedMovieTitle) {
-      this.fetchMovieData();
-      //this if statement launches the fetch function JUST when the selectedMovieTitle
-      //changes from the props, NOT when the state  is being set one more time
-    }
-  }
-  fetchMovieData = async () => {
+  //   //every time you use componentDidUpdate, you have to put a constraint!
+  //   //you want to pull handbrake
+  //   if (prevProps.selectedMovieTitle !== this.props.selectedMovieTitle) {
+  //     this.fetchMovieData();
+  //     //this if statement launches the fetch function JUST when the selectedMovieTitle
+  //     //changes from the props, NOT when the state  is being set one more time
+  //   }
+  // }
+
+  useEffect(() => {
+    fetchMovieData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.selectedMovieTitle]);
+
+  const fetchMovieData = async () => {
     try {
       let response = await fetch(
-        "http://www.omdbapi.com/?apikey=d54c1d6b&s=" +
-          this.props.selectedMovieTitle
+        "http://www.omdbapi.com/?apikey=d54c1d6b&s=" + props.selectedMovieTitle
       );
       console.log(response);
       if (response.ok) {
         let data = await response.json();
         let chosenMovieInfo = data.Search[0];
         console.log("chosen movie info:", chosenMovieInfo);
-        this.setState({
-          selectedMovieObject: chosenMovieInfo,
-          isLoading: false,
-        });
+        // this.setState({
+        //   selectedMovieObject: chosenMovieInfo,
+        //   isLoading: false,
+        // });
+        setSelectedMovieObject(chosenMovieInfo);
+        setIsLoading(false);
       } else {
         console.log("Something went wrong :(");
-        this.setState({ isLoading: false });
+        // this.setState({ isLoading: false });
+        setIsLoading(false);
       }
     } catch (error) {
       console.log(error);
-      this.setState({ isLoading: false });
+      // this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  render() {
-    return (
-      <div>
-        {/* if we don't use ternary operator here, the page will be broken, because selectedMovieObject is null for start */}
-        {this.state.isLoading ? (
-          <Spinner animation="border" variant="warning" />
-        ) : (
-          <Card>
-            <Card.Img
-              variant="top"
-              src={this.state.selectedMovieObject.Poster}
-            />
-            <Card.Body>
-              <Card.Title>{this.state.selectedMovieObject.Title}</Card.Title>
-              <Card.Text>
-                {this.state.selectedMovieObject.Year} -{" "}
-                {this.state.selectedMovieObject.imdbID}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      {/* if we don't use ternary operator here, the page will be broken, because selectedMovieObject is null for start */}
+      {isLoading ? (
+        <Spinner animation="border" variant="warning" />
+      ) : (
+        <Card>
+          <Card.Img variant="top" src={selectedMovieObject.Poster} />
+          <Card.Body>
+            <Card.Title>{selectedMovieObject.Title}</Card.Title>
+            <Card.Text>
+              {selectedMovieObject.Year} - {selectedMovieObject.imdbID}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </div>
+  );
+};
 
 export default MovieCard;
